97a0e605f9a33e7cdf3dd1208801236d4417576d
Abdul Rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-05-02 21:07:05 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# object-detection
+ 2nd sem project 
---
+++ src/test/java/com/example/object/detection/ObjectDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.object.detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
